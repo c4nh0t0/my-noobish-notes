8607287d20dd3890819185d507df7acc62d29ee2
c4nh0t0 <diego.ghb47@gmail.com>
2020-10-08 22:23:49 -0300

    Update vultrscript.sh

--- vultrscript.sh
+++ vultrscript.sh
@@ -7,4 +7,11 @@ chmod 700 /root/.ssh/authorized_keys
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 systemctl reload sshd
 
-apt update -y && apt upgrade -y && apt full-upgrade -y && apt dist-upgrade -y && apt --fix-broken install -y && apt install -y software-properties-common apt-transport-https build-essential checkinstall autoconf automake make g++ unzip flex bison gcc wget curl dnsutils nmap jq tmux tcpdump gufw libssl-dev libtool libpcap-dev python3 python3-pip
+# Updating, Upgrading, Cleaning Up and Installing some essential tools
+apt update -y && apt upgrade -y && apt full-upgrade -y && apt dist-upgrade -y && apt --fix-broken install -y && apt install -y software-properties-common apt-transport-https build-essential checkinstall autoconf automake make g++ unzip flex bison gcc wget curl dnsutils nmap jq tmux tcpdump ufw libssl-dev libtool libpcap-dev python3 python3-pip xdg-utils pkg-config vim chromium-browser iptraf
+
+# Downloading and Installing "altdns"
+cd /opt && git clone https://github.com/infosec-au/altdns.git && cd altdns/ && pip install -r requirements.txt
+
+# Downloading and Installing "aquatone"
+cd /opt && mkdir aquatone && cd aquatone/ && wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip && unzip aquatone_linux_amd64_1.7.0.zip
